9677a6cef71c80079003f3ab2a92e67bc3346c1b
-MonzterMarkz- <phongsakorn801@gmail.com>
2024-04-27 15:43:08 +0700

    Add all files of Project
    Hello World :+1:

---
+++ app.mjs
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+import express from 'express';
+import debug from 'debug';
+import morgan from 'morgan';
+
+const deb = debug('app')
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) => {
+    res.send('Hello Mark Phongsakorn');
+})
+
+app.listen(port, () => {
+    // console.log("Listening on port: " + chalk.green(port));
+    deb("Listening on port: " + chalk.green(port));
+})
