9843c9e16feacd5bc201816d9ea649ba3499e98f
-MonzterMarkz- <phongsakorn801@gmail.com>
2024-04-27 16:52:17 +0700

    use template free and Set npm script

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const chalk = require('chalk');
+const debug = require('debug')('app')
+const morgan = require('morgan');
+const path = require('path');
+
+// const deb = debug('app')
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.get("/", (req, res) => {
+    res.send('Hello Mark Phongsakorn');
+})
+
+app.listen(port, () => {
+    // console.log("Listening on port: " + chalk.green(port));
+    debug("Listening on port: " + port);
+})
